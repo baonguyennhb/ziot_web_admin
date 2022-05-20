d83e8bf6b116787edb9489b8ab15efc09e0975c0
HUU BAO <bao.nguyen@gmail.com>
2022-05-20 22:32:40 +0700

    admin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import  Layout  from "./component/Layout";
+import Layout from "./component/Layout";
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from "./component/Login/Login";
@@ -8,16 +8,24 @@ import Device from "./component/Device";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element = {<Login/>}/>
-    </Routes>
-    <Layout>
       <Routes>
-        <Route path='/user' element = {<UserPage/>} />
-        <Route path='/site' element = {<Site/>}/>
-        <Route path='/Device' element = {<Device/>}/>
+        <Route exact path="/" element={<Login />} />
+        <Route path='/user' element={
+          <Layout>
+            <UserPage />
+          </Layout>
+        } />
+        <Route path='/site' element={
+          <Layout>
+            <Site />
+          </Layout>
+        } />
+        <Route path='/device' element={
+          <Layout>
+            <Device />
+          </Layout>
+        } />
       </Routes>
-    </Layout>
     </BrowserRouter>
   );
 }
